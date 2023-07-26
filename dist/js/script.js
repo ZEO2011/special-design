5b50b787d820a3cf3b24fd1b251ba7a36c984d8f
Ziad hatem <hatemziad50@gmail.com>
2023-07-26 10:21:18 +0300

    completed color setting and random background and landing and bullets setting and reset options design

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,9 +1,88 @@
 "use strict";
+let html = document.querySelector("html");
 let menu = document.getElementById("menu");
-let links = document.querySelector("nav .links");
+let menuIcon = document.querySelector("#menu i");
+let linksContainer = document.querySelector("nav .links");
+let links = document.querySelectorAll("nav .links li");
+let landingBg = document.querySelector(".landing");
+let settings = document.querySelector(".settings-box");
+let settingsIcon = document.querySelector(".settings-box i");
+let colorSetting = document.querySelectorAll(".settings-box .colors li");
+let randomBgSetting = document.querySelectorAll(".settings-box .randomBg button");
+let bulletsSetting = document.querySelectorAll(".settings-box .bullets button");
+let images = ["01.jpg", "02.jpg", "03.jpg", "04.jpg"];
+let randomImageInt = setInterval(() => {
+    let randomNum = Math.floor(Math.random() * images.length);
+    if (landingBg !== null)
+        landingBg.style.backgroundImage = `url(../../src/assets/imgs/landing/${images[randomNum]})`;
+}, 10000);
+if (localStorage.getItem("main-color") !== null) {
+    let savedColor = eval(`${localStorage.getItem("main-color")}`);
+    html === null || html === void 0 ? void 0 : html.style.setProperty("--main-color", savedColor);
+    colorSetting.forEach((button) => {
+        if (button.getAttribute("data-color") === savedColor) {
+            colorSetting.forEach((button) => button.classList.remove("active"));
+            button.classList.add("active");
+        }
+    });
+}
+function removeAllExcept(status) {
+    randomBgSetting.forEach((button) => {
+        button.classList.remove("active");
+        if (button.getAttribute("random") === status) {
+            button.classList.add("active");
+        }
+    });
+}
+if (localStorage.getItem("randomImage") !== null) {
+    if (localStorage.getItem("randomImage") === "false") {
+        clearInterval(randomImageInt);
+        removeAllExcept("false");
+    }
+    else {
+        removeAllExcept("true");
+    }
+}
 if (menu !== null)
     menu.onclick = function () {
-        if (links !== null)
-            links.classList.toggle("show");
+        if (linksContainer !== null)
+            linksContainer.classList.toggle("show");
+        if (menuIcon !== null)
+            menuIcon.classList.toggle("fa-xmark");
     };
+if (settingsIcon !== null)
+    settingsIcon.onclick = () => settings === null || settings === void 0 ? void 0 : settings.classList.toggle("show");
+function settingsFunc(parent) {
+    parent.forEach((button) => {
+        button.addEventListener("click", function () {
+            parent.forEach((button) => button.classList.remove("active"));
+            button.classList.add("active");
+        });
+    });
+}
+colorSetting.forEach((button) => {
+    settingsFunc(colorSetting);
+    button.addEventListener("click", function (event) {
+        html === null || html === void 0 ? void 0 : html.style.setProperty("--main-color", button.getAttribute("data-color"));
+        localStorage.setItem("main-color", JSON.stringify(button.getAttribute("data-color")));
+    });
+});
+randomBgSetting.forEach((button) => {
+    settingsFunc(randomBgSetting);
+    button.addEventListener("click", function () {
+        if (button.getAttribute("random") === "false") {
+            clearInterval(randomImageInt);
+            localStorage.setItem("randomImage", "false");
+        }
+        else {
+            setInterval(() => {
+                let randomNum = Math.floor(Math.random() * images.length);
+                if (landingBg !== null)
+                    landingBg.style.backgroundImage = `url(../../src/assets/imgs/landing/${images[randomNum]})`;
+            }, 10000);
+            localStorage.setItem("randomImage", "true");
+        }
+    });
+});
+settingsFunc(bulletsSetting);
 //# sourceMappingURL=script.js.map
--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,9 +1,134 @@
 // Variables
+let html: HTMLHtmlElement | null = document.querySelector("html");
 let menu: HTMLElement | null = document.getElementById("menu");
-let links: HTMLElement | null = document.querySelector("nav .links");
+let menuIcon: HTMLElement | null = document.querySelector("#menu i");
+let linksContainer: HTMLElement | null = document.querySelector("nav .links");
+let links: NodeListOf<Element> = document.querySelectorAll("nav .links li");
+let landingBg: HTMLElement | null = document.querySelector(".landing");
+let settings: HTMLElement | null = document.querySelector(".settings-box");
+let settingsIcon: HTMLElement | null =
+	document.querySelector(".settings-box i");
+let colorSetting: NodeListOf<Element> = document.querySelectorAll(
+	".settings-box .colors li",
+);
+let randomBgSetting: NodeListOf<Element> = document.querySelectorAll(
+	".settings-box .randomBg button",
+);
+let bulletsSetting: NodeListOf<Element> = document.querySelectorAll(
+	".settings-box .bullets button",
+);
+
+// Random background
+
+// randomImage function
+
+// array of images
+
+let images: string[] = ["01.jpg", "02.jpg", "03.jpg", "04.jpg"];
+
+let randomImageInt = setInterval(() => {
+	let randomNum: number = Math.floor(Math.random() * images.length);
+	if (landingBg !== null)
+		landingBg.style.backgroundImage = `url(../../src/assets/imgs/landing/${images[randomNum]})`;
+}, 10000);
+
+// check if there is any saved color
+
+if (localStorage.getItem("main-color") !== null) {
+	let savedColor = eval(`${localStorage.getItem("main-color")}`);
+	html?.style.setProperty("--main-color", savedColor);
+	colorSetting.forEach((button) => {
+		if (button.getAttribute("data-color") === savedColor) {
+			colorSetting.forEach((button) =>
+				button.classList.remove("active"),
+			);
+			button.classList.add("active");
+		}
+	});
+}
+
+// check if user wasn't wan't to random image
+
+// make clicked button active
+function removeAllExcept(status: string) {
+	randomBgSetting.forEach((button) => {
+		button.classList.remove("active");
+		if (button.getAttribute("random") === status) {
+			button.classList.add("active");
+		}
+	});
+}
+
+if (localStorage.getItem("randomImage") !== null) {
+	if (localStorage.getItem("randomImage") === "false") {
+		// clear interval of random image
+		clearInterval(randomImageInt);
+		removeAllExcept("false");
+	} else {
+		removeAllExcept("true");
+	}
+}
+
 // Toggle menu
 
 if (menu !== null)
 	menu.onclick = function () {
-		if (links !== null) links.classList.toggle("show");
+		if (linksContainer !== null) linksContainer.classList.toggle("show");
+		if (menuIcon !== null) menuIcon.classList.toggle("fa-xmark");
 	};
+
+// Settings box
+
+if (settingsIcon !== null)
+	settingsIcon.onclick = () => settings?.classList.toggle("show");
+
+// options boxes
+
+// setting active class to clicked button
+function settingsFunc(parent: NodeListOf<Element>) {
+	parent.forEach((button: Element) => {
+		button.addEventListener("click", function () {
+			parent.forEach((button: Element) =>
+				button.classList.remove("active"),
+			);
+			button.classList.add("active");
+		});
+	});
+}
+
+colorSetting.forEach((button: Element) => {
+	settingsFunc(colorSetting);
+	button.addEventListener("click", function (event) {
+		// set the data-color to html element
+		html?.style.setProperty(
+			"--main-color",
+			button.getAttribute("data-color"),
+		);
+		// save chosen color
+		localStorage.setItem(
+			"main-color",
+			JSON.stringify(button.getAttribute("data-color")),
+		);
+	});
+});
+
+randomBgSetting.forEach((button: Element) => {
+	settingsFunc(randomBgSetting);
+	button.addEventListener("click", function () {
+		if (button.getAttribute("random") === "false") {
+			// clear interval of random image & save it in localStorage
+			clearInterval(randomImageInt);
+			localStorage.setItem("randomImage", "false");
+		} else {
+			setInterval(() => {
+				let randomNum: number = Math.floor(
+					Math.random() * images.length,
+				);
+				if (landingBg !== null)
+					landingBg.style.backgroundImage = `url(../../src/assets/imgs/landing/${images[randomNum]})`;
+			}, 10000);
+			localStorage.setItem("randomImage", "true");
+		}
+	});
+});
+settingsFunc(bulletsSetting);
